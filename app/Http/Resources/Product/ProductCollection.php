eaba30fae529d929f16de858242c91bf022526a2
Imran <imranh.cse.ru@gmail.com>
2019-03-16 13:33:36 +0600

    'Product_Collection_Resource'

--- app/Http/Resources/Product/ProductCollection.php
+++ app/Http/Resources/Product/ProductCollection.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Resources\Product;
 
-use Illuminate\Http\Resources\Json\ResourceCollection;
+use Illuminate\Http\Resources\Json\Resource;
 
-class ProductCollection extends ResourceCollection
+class ProductCollection extends Resource
 {
     /**
      * Transform the resource collection into an array.
@@ -14,6 +14,13 @@ class ProductCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'name' => $this->name,
+            'totalPrice' => ceil($this->price - $this->price*$this->discount/100),
+            'rating' => $this->review->count() > 0 ? round($this->review->sum('star')/$this->review->count(),1) : 'No rating Yet',
+            'href' => [
+                'detail' => route('products.show',$this->id)
+            ]
+        ];
     }
 }
